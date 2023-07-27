384239fd777fe20e3c964bcbc0c29f4317553168
Nathan Ladd <nathanladd@gmail.com>
2023-07-27 18:41:23 -0500

    Scripts are updated

--- get-sources.sh
+++ get-sources.sh
@@ -10,10 +10,13 @@ echo
 echo "Start ($(basename "$0"))"
 
 echo
-echo "Updating Sources"
+echo "Getting Sources"
 echo "= = ="
 
 cmd="gem install --force --no-document --install-dir ./sources --bindir ./sources/bin --no-user-install test_bench"
 
 echo $cmd
 ($cmd)
+
+echo
+echo "Done ($(basename "$0"))"
--- update-lib.sh
+++ update-lib.sh
@@ -17,34 +17,48 @@ rm -rf lib
 
 mkdir -p lib/test_bench_isolated
 
-for input_file in $(find sources/gems/*/lib -type f); do
-  file=${input_file/#sources\/gems\/*\/lib/lib\/test_bench_isolated}
+for gem_dir in sources/gems/*; do
+  for input_file in $(find $gem_dir -type f); do
+    file=${input_file/#$gem_dir\//}
+    file=${file/#lib/lib\/test_bench_isolated}
 
-  mkdir -p $(dirname $file)
+    mkdir -p $(dirname $file)
 
-  ed --quiet --verbose --extended-regexp --loose-exit-status $input_file <<ED
+    ed --quiet --verbose --extended-regexp $input_file <<ED
 #
-# Update require statements
+## Update require statements
 ,g/^require '.*'$/s/^require '(.*)'$/require 'test_bench_isolated\/\1'/
 #
-# Find all outermost module and class declarations, then indent every line, ...
+## Find all outermost module and class declarations, then indent every line, ...
 ,g/^(module|class)/,s/^/  /\\
-# ..., then prepend 'module TestBenchIsolated', ...\\
+## ..., then prepend 'module TestBenchIsolated', ...\\
 0i\\
 module TestBenchIsolated\\
 .\\
-# ..., then append 'end'\\
+## ..., then append 'end'\\
 \$a\\
 end
 #
-# Write file
+## Replace TestBench::CLI with TestBenchIsolated::TestBench::CLI
+,g/^TestBench::CLI\.\(\)$/c\\
+TestBenchIsolated::TestBench::CLI.()
+#
+## Write file
 w $file
 ED
 
-  if [ -f $file ]; then
-    echo $file
-  else
-    echo -e "\e[1;31mError: couldn't write $file\e[39;22m"
-    false
-  fi
+    if [ -f $file ]; then
+      echo $file
+    else
+      echo -e "\e[1;31mError: couldn't write $file\e[39;22m"
+      false
+    fi
+  done
 done
+
+echo
+echo "Running CLI"
+echo "- - -"
+ruby --disable-gems -r./init.rb -e 'TestBenchIsolated::TestBench::CLI.()' || true
+
+echo "Done ($(basename "$0"))"
